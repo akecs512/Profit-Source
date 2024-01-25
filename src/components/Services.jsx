50a89092e6713e89433248c98b79d4b3f4a0078c
AK <AK@adriennes-mbp.mynetworksettings.com>
2024-01-25 11:15:29 -0600

    about page, services btns, clean up images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ function Services() {
         <div className="service-btn flex justify-center">
             <button id="btn-1" className="btn btn-wide btn-primary rounded-tl-lg">Bookkeeping Services</button>
             <button id="btn-2"className="btn btn-wide bg-green-600 rounded-none">Payroll Services</button>
-            <button id="btn-3"className="btn btn-wide bg-green-700 rounded-none">Tax Services</button>
+            <button id="btn-3"className="btn btn-wide bg-green-700 rounded-none">Document Services</button>
             <button id="btn-4" className="btn btn-wide bg-green-800 rounded-tr-lg">Analysis & Reporting</button>
             </div>
         </>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,62 +1,58 @@
-import {
-  skillIcons,
-  frameworkIcons,
-  toolsIcons,
-} from "../lib/content/skillIcons.js";
-import SkillsCard from "/src/components/SkillsCard.jsx";
+
 
 export const AboutPage = () => {
   return (
     <>
       <h1>About Profit Source Advance Bookkeeping</h1>
-      <div className="about-card flex my-10 lg:card-side bg-base-100">
-      {/* <figure className="about-image"> */}
-          <img className="about w-36 h-36" src="/imagesNew/karenTodd.jpeg" alt="Me Image" />
-          {/* </figure> */}
-   
-        <div className="about-card-body flex flex-col justify-center pr-50px">
-          {/* <h2 className="card-title">New album is released!</h2> */}
-          <p>
-            {" "}
-            As a team player, Profit Source provides these financial management services:
-            Setup/Maintenance of accounting, document management, and customer management, database software, and/or software (often integrated with accounting software) time and project tracking solutions
-            o	QuickBooks Online ProAdvisor
-            o	QuickBooks Desktop Expert - Enterprise, Warehouse, Pro, Premier, and Mac, working remotely using either client’s server or Right Networks,
-            o	Xero Certified
-            o	FreshBooks Certified
-            o	Intacct – an ERP cloud solution for larger companies
-            o	A myriad of document, customer, and database management solutions are available, but we will find the best solution for your business needs
-            Setup or clean-up Chart of Accounts with appropriate account numbers
-            Preparation/clean-up financials for tax-readiness for tax year and ongoing for precise visibility for decision-makers
-            “Back-office” Bookkeeping work including processing all Payables and Receivables, including entering vendor’s bills and customer invoices, collections calls and/or serve as a contact for billing
-            Manage payroll, file all payroll tax returns, and submit payments
-            o	Quickly resolve payroll tax issues if behind
-            o	Vacation / Sick pay tracking
-            o	Assist to find affordable health care plans
-            o	Assess ACA Compliance
-            Manage sales tax returns, 1099 & 1096
-            Cost Management to determine cost behavior, appropriate prices, and/or an effective budget
-            Inventory and/or asset tracking
-            Regular reporting, including optional extensive analysis to determine risks and strengths
-            Preparation of simple, complex, and attractive presentations using PowerPoint for investors and decision-makers
-            We facilitate the relationship between the client and the CPA:
-            **We understand the difficulties faced by CPAs in that documents and answers to questions are often hard to obtain, and are at times given at the last minute, causing a rush of work at tax deadlines, and we offer to CPAs the ability to facilitate that relationship and send all information and paperwork needed to support the tax return.
-            Completed and full financial statement review and reconciliation, so that you can quickly tie balance sheet accounts to workpapers.
-            Provide all necessary workpapers for each Balance Sheet Account, including bank statements, detail and documents for all acquisitions & disposals, A/R and A/P detail at year-end, payroll reports to support all Wages and Employee taxes as well as year-end liabilities, Loan documents, sales tax liability report for year-end, etc. etc.
-            Ready to answer any/all questions immediately. No need to wait for client response.
-            In the event that a CPA hired Profit Source, we can either bill the client directly or bill the CPA firm.
-            Consulting Services for small to large businesses
-            Assist your start-up to establish business filings, marketing, and business plans
-            Assess internal controls, propose efficiency improvements, and workflow procedures
-            Research your business to present the best options for passive and active marketing
-            Create advertising and marketing strategies and help to implement plan
-          </p>
+
+
+      <div className="card card-side text-black">
+        <img className="about-image" src="/imagesNew/karenTodd.jpeg" alt="Me Image" />
+        <div className="card-body">
+          <p>  {" "}
+            Profit Source offers the small to medium sized business an exceptional suite of bookkeeping services to maximize profit and ensure success.
+            We work with you to provide the right financial management services to meet your needs.
+            Some of the services we offer are:
+            <ul className="list-disc">
+              <li>Setup or maintenance of accounting and software</li>
+              <li>Document and customer management</li>
+              <li>Database software solutions</li>
+              <li>Time and project tracking solutions</li>
+              <li>Preparation/clean-up financial documents for tax-readiness</li>
+              <li>Manage payroll</li>
+              <li>Facilitate client/CPA relationship</li>
+              <li>Consulting Services</li>
+            </ul></p>
         </div>
       </div>
+
+
+
       <h1 className="my-10" id="skills">
-        Skills
+        Certifications
       </h1>
-    
+      <div className="card text-black">
+        <div className="card-body items-center">
+          <p>
+            <ul className="list-disc">
+              <li>QuickBooks Online ProAdvisor</li>
+              <li>QuickBooks Desktop Expert</li>
+              <li>Xero Certified</li>
+              <li>FreshBooks Certified</li>
+              <li>Intacct</li>
+            </ul>
+          </p>
+          <div className="certs flex h-20">
+            <img className="cert-image h-20" src="/imagesNew/QuickbooksBadges.png" alt="Quickbooks Cert" />
+            <img className="cert-image h-20" src="/imagesNew/xeroBadge.png" alt="Xero Cert" />
+            <img className="cert-image h-20" src="public/imagesNew/freshbooksBadge.png" alt="Freshbooks Cert" />
+            <img className="cert-image h-20" src="/imagesNew/intacctBadge.png" alt="Intacct Cert" />
+
+          </div>
+        </div>
+      </div>
+
+
     </>
   );
 };
